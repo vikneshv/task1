9b154dc1e7186a1450c4d095684aa7573e169c5f
viknesh <vickypondi69@gmail.com>
2017-11-17 11:18:19 +0530

    new 2

---
+++ js/list.js
@@ -0,0 +1,31 @@
+var API_KEY = "https://jsonplaceholder.typicode.com/todos";
+
+(function  {
+    $.getJSON(API_KEY).then(function (data) {
+        $.each(data, function (key, value) {
+            $("#todolist").prepend('<li><input type="checkbox" id="mycheckbox" class="done"/>' + value.title + ' <span class="glyphicon glyphicon-ok"></span>=</li>');
+            $("#mycheckbox").prop('checked', value.completed);
+        });
+    });
+})(jQuery);
+
+$('#myInput').keypress(function (event) {
+    event.stopPropagation();
+ var keycode = (event.keyCode ? event.keyCode : event.which);
+    if (keycode == '13') {
+        console.log('You pressed "enter key" in textbox');
+        if ($(this).val() != '') {
+            var todo = $(this).val();
+            console.log(todo);
+            createTodo(todo);
+        } else {
+
+        }
+    }
+});
+
+function createTodo(text) {
+    var markup = '<li><input type="checkbox" id="mycheckbox" class="done"/>' + text + '<span class="glyphicon glyphicon-pencil"></span></li>';
+    $('#todolist').prepend(markup);
+    $("#myInput").val('');
+}
